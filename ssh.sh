2924543e53ffd88741df2c94c2c27b225f03fc75
Replit user <>
2022-09-29 11:10:20 +0000

    Updated ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,18 @@
-if [[ -z $1 ]]; then
-    echo Specify which number to ssh into
+#!/bin/bash
+
+set -e
+
+if [ -z $1 ]; then
+    read -r -p "Which user do you want to sign into? " which_user
+else
+    which_user="$1"
+fi
+
+if [ ! -f "$which_user.txt" ]; then
+    echo "No password for user yet"
     exit 1
 fi
 
-ssh "bandit$1@bandit.labs.overthewire.org" -p 2220
+cat "$which_user.txt"
+
+ssh "bandit$which_user@bandit.labs.overthewire.org" -p 2220
